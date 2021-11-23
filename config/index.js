720ec211eb2cee6addc76aba9979f8f83b5ae479
Haseeb Udeen <haseebbajwa1@outlook.com>
2021-11-23 11:07:11 +0530

    changed to https

--- config/index.js
+++ config/index.js
@@ -1,3 +1,3 @@
-export const API_URL = "http://exam105_backend:9090";
+export const API_URL = "https://exam105_backend:9090";
 // export const API_URL = process.env.NEXT_PUBLIC_API_URL;
 export const MEASUREMENT_ID = process.env.NEXT_PUBLIC_MEASUREMENT_ID;
