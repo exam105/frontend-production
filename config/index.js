8097bbbb525a8dc7e7dbafa5b7b6f462aaf11832
Haseeb Udeen <haseebbajwa1@outlook.com>
2021-11-24 15:09:38 +0530

    changed the https to http in backend url

--- config/index.js
+++ config/index.js
@@ -1,4 +1,4 @@
 export const API = "/api/proxy";
-export const API_URL = "https://exam105_backend:9090";
+export const API_URL = "http://exam105_backend:9090";
 // export const API_URL = process.env.NEXT_PUBLIC_API_URL;
 export const MEASUREMENT_ID = process.env.NEXT_PUBLIC_MEASUREMENT_ID;
