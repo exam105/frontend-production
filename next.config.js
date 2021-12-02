1aa03b70c6aa1a39be2ea22097eb785ec3ef09db
Haseeb Udeen <haseebbajwa1@outlook.com>
2021-12-02 11:00:14 +0530

    added security headers

--- next.config.js
+++ next.config.js
@@ -1,6 +1,42 @@
+const headers = [
+  {
+    key: "X-DNS-Prefetch-Control",
+    value: "on",
+  },
+  {
+    key: "Strict-Transport-Security",
+    value: "max-age=63072000; includeSubDomains; preload",
+  },
+  {
+    key: "Server",
+    value: "Apache", // phony server
+  },
+  {
+    key: "X-Content-Type-Options",
+    value: "nosniff",
+  },
+  {
+    key: "X-Frame-Options",
+    value: "sameorigin",
+  },
+  {
+    key: "X-XSS-Protection",
+    value: "1; mode=block",
+  },
+  {
+    key: "Referrer-Policy",
+    value: "same-origin",
+  },
+  {
+    key: "Permissions-Policy",
+    value: "geolocation=*", // allow specified policies here
+  },
+];
+
 module.exports = {
   reactStrictMode: true,
   images: {
     domains: ["exam105.s3-ap-southeast-1.amazonaws.com"],
   },
+  headers,
 };
