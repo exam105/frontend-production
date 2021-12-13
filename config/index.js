85ccdeae6a8317584912f315f7b11379bbc71a0f
Haseeb Udeen <haseebudeen@outlook.com>
2021-12-13 13:09:52 +0530

    merged two branches and added feature improvements and bug fixes

--- config/index.js
+++ config/index.js
@@ -1,9 +1,4 @@
 export const API = "/api/proxy";
-// export const API_URL = "http://exam105_backend:9090";
 export const API_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
-// export const API_URL = process.env.NEXT_PUBLIC_API_URL;
 export const MEASUREMENT_ID =
   process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_MEASUREMENT_ID;
-// export const shell = process.env.SHELL;
-// export const pwd = process.env.PWD;
-// export const logname = process.env.LOGNAME;
