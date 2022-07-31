29623eb728016f8da3c0b09a9e8faf2d9f948600
RacketyWater7 <haseebudeen@outlook.com>
2022-07-31 15:27:22 +0500

    added reveal and not reveal feature, and removed the imagesSliderModal

--- components/SingleQuestion/SingleQuestion.js
+++ components/SingleQuestion/SingleQuestion.js
@@ -1,10 +1,11 @@
 import { ImageSliderModal, Modal } from "@components/common/Modals";
 import Image from "next/image";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import styles from "./SingleQuestion.module.css";
 import { MathpixMarkdown, MathpixLoader } from "mathpix-markdown-it";
 import Loader from "@components/common/Loader";
 import { FiCopy } from "react-icons/fi";
+import { FaCircle } from "react-icons/fa";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
@@ -20,6 +21,13 @@ function SingleQuestion({
   const [showImageSliderModal, setShowImageSliderModal] = useState(false);
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
   const [ansImagesSlider, setAnsImagesSlider] = useState(false);
+  const [reveal, setReveal] = useState(false);
+
+  useEffect(() => {
+    if (localStorage.getItem("reveal") === "true") {
+      setReveal(true);
+    }
+  }, []);
 
   const shareQuestion = () => {
     navigator.clipboard.writeText(window.location.href);
@@ -91,13 +99,18 @@ function SingleQuestion({
                       <div
                         key={index}
                         className={styles.overviewcard}
-                        onClick={() => {
-                          setAnsImagesSlider(false);
-                          setCurrentImageIndex(index);
-                          setShowImageSliderModal(true);
-                        }}
+                        // onClick={() => {
+                        //   setAnsImagesSlider(false);
+                        //   setCurrentImageIndex(index);
+                        //   setShowImageSliderModal(true);
+                        // }}
                       >
                         <div className={styles.cardImageContainer}>
+                          {/* <img
+                            src="https://exam105.s3-ap-southeast-1.amazonaws.com/Chemistry/Igcse_edexcel_1_2012_paper2_ques_4(c).jpg"
+                            alt="blah"
+                            className={styles.cardImage}
+                          /> */}
                           <img
                             src={image.imageurl}
                             alt={image.imageurl}
@@ -118,13 +131,41 @@ function SingleQuestion({
                   <p className={styles.questionText}>
                     {data.answer ? "Answer:" : "Options:"}
                   </p>
+                  <span className={styles.revealCheckbox}>
+                    <button
+                      onClick={() => {
+                        setReveal(!reveal);
+                        localStorage.setItem("reveal", !reveal);
+                      }}
+                      style={
+                        reveal
+                          ? { color: "#268dec", backgroundColor: "#ffffff" }
+                          : { color: "#ffffff", backgroundColor: "#268dec" }
+                      }
+                      className={styles.revealBtn}
+                    >
+                      {data.options ? (
+                        <>
+                          {reveal
+                            ? "Hide the correct option"
+                            : "Reveal the correct option"}
+                        </>
+                      ) : (
+                        <>{reveal ? "Hide the answer" : "Reveal the answer"}</>
+                      )}
+                    </button>
+                  </span>
                 </div>
 
                 <div className={styles.answerText}>
                   {data.answer ? (
-                    <MathpixLoader>
-                      <MathpixMarkdown text={data.answer} />
-                    </MathpixLoader>
+                    <>
+                      {reveal && (
+                        <MathpixLoader>
+                          <MathpixMarkdown text={data.answer} />
+                        </MathpixLoader>
+                      )}
+                    </>
                   ) : (
                     <div className={styles.options}>
                       {data.options?.map((option, index) => (
@@ -132,25 +173,38 @@ function SingleQuestion({
                           key={index}
                           style={{
                             display: "flex",
-
                             paddingTop: "10px",
                           }}
                         >
-                          {option.correct === true ? (
-                            <Image
-                              src="/images/check-new.svg"
-                              width="24"
-                              height="0"
-                              alt="correct"
-                            />
-                          ) : (
-                            <Image
-                              src="/images/wrong.svg"
-                              width="22"
-                              height="5"
-                              alt="wrong"
-                              className="wrong"
-                            />
+                          {reveal === true && (
+                            <>
+                              {option.correct === true ? (
+                                <Image
+                                  src="/images/check-new.svg"
+                                  width="24"
+                                  height="0"
+                                  alt="correct"
+                                />
+                              ) : (
+                                <Image
+                                  src="/images/wrong.svg"
+                                  width="22"
+                                  height="5"
+                                  alt="wrong"
+                                  className="wrong"
+                                />
+                              )}
+                            </>
+                          )}
+                          {reveal === false && (
+                            <div style={{ marginTop: "10px" }}>
+                              <FaCircle
+                                width={22}
+                                height={22}
+                                className="wrong"
+                                color="#2e382e"
+                              />
+                            </div>
                           )}
                           &nbsp;&nbsp;
                           <p
@@ -170,24 +224,29 @@ function SingleQuestion({
                 </div>
               </div>
               <p className={styles.questionText}>
-                {ansImages && ansImages.length > 0 ? "Images:" : ""}
+                {ansImages && ansImages.length > 0 && reveal ? "Images:" : ""}
               </p>
             </div>
 
             <div className={styles.mainOverview}>
-              {ansImages?.length !== 0
+              {ansImages?.length !== 0 && reveal
                 ? ansImages?.map((image, index) => {
                     return (
                       <div
                         key={index}
                         className={styles.overviewcard}
-                        onClick={() => {
-                          setAnsImagesSlider(true);
-                          setCurrentImageIndex(index);
-                          setShowImageSliderModal(true);
-                        }}
+                        // onClick={() => {
+                        //   setAnsImagesSlider(true);
+                        //   setCurrentImageIndex(index);
+                        //   setShowImageSliderModal(true);
+                        // }}
                       >
                         <div className={styles.cardImageContainer}>
+                          {/* <img
+                            src="https://exam105.s3-ap-southeast-1.amazonaws.com/Chemistry/Igcse_edexcel_1_2012_paper2_ques_4(c).jpg"
+                            alt="blah"
+                            className={styles.cardImage}
+                          /> */}
                           <img
                             src={image.imageurl}
                             alt={image.imageurl}
